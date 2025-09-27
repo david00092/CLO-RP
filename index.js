f5389cf0fb1eb4897ce301557ac99fb56c8f3d8c
david00092 <lipe03072@gmail.com>
2025-09-26 23:54:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ process.on("unhandledRejection", (reason, promise) => {
 
 // Configurações principais
 const client = new Client({
-  intents: 
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
@@ -34,16 +34,16 @@ const client = new Client({
 const prefix = process.env.PREFIX || "!";
 const authorizedBotIDs = ["411916947773587456", "987654321098765432"]; // IDs bots autorizados
 const allowedRoles = ["1406868774434504731", "1406868774434504730"]; // IDs dos cargos autorizados a usar comandos
-const logChannelId = "1406868774807928969"; // ID do canal de logs
+const logChannelId = "1398886461025030235"; // ID do canal de logs
 const autoRoleId = "1406868774417858632"; // ID do cargo automático ao entrar
 const ticketSupportRoleId = "1406868774417858637"; // cargo que pode fechar ticket
 
 // Tickets
 const ticketOpenChannelId = "1406868775869087760"; // canal do menu para abrir tickets
 const ticketCategoryIds = {
-  vendas: "1406868776267284548",
-  suporte: "1406868776267284547",
-  denuncia: "1406868776267284546",
+  vendas: "1406868776452096057",
+  suporte: "1406868776452096056",
+  denuncia: "1406868776452096058",
 };
 
 // Servidor HTTP para uptime (ex: UptimeRobot)
